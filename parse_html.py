9debac3a27aac905a5bbe6ac6c28380afbd81afd
Hayley Denbraver <runningwendybird@gmail.com>
2015-05-11 15:23:20 -0700

    Improved print out of list of links using regex.

--- parse_html.py
+++ parse_html.py
@@ -1,13 +1,12 @@
 import urllib2
+import re
+
 
 def return_links(my_url):
-	f = urllib2.urlopen(my_url)
-	links = []
-	link_indicator = "href"
-	for line in f:
-		split_line = line.split()
-		for element in split_line:
-			if link_indicator in element:
-				links.append(element)
+	s = urllib2.urlopen(my_url)
+	s = s.read()
+	urls = re.findall(r'href=[\'"]?([^\'" >]+)', s)
+	return urls
 
-	return links
+
+
