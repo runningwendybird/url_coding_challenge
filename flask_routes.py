3b79fd27a76eabb698f7796570b41cd18c8a1698
Hayley Denbraver <runningwendybird@gmail.com>
2015-05-11 13:33:46 -0700

    Basic genet server set up. Shows a basic form on the home page asking for a url.

---
+++ flask_routes.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, render_template, redirect, url_for, flash, jsonify
+import jinja2
+
+app = Flask(__name__)
+app.secret_key = "TESTINGKEY"
+app.jinja_env.undefined = jinja2.StrictUndefined
+
+@app.route("/")
+def landing_page():
+	return render_template("home.html")
