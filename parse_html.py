c7c27da677b3b05c5283559c82f3ffd78e68cae8
Hayley Denbraver <runningwendybird@gmail.com>
2015-05-12 07:49:45 -0700

    Fixed determine_domain function to remove 'www.' if present.

--- parse_html.py
+++ parse_html.py
@@ -13,6 +13,11 @@ def return_links(my_url):
 def determine_domain(my_url):
 	"""Returns the domain for a given url."""
 	domain = urlparse.urlparse(my_url)[1]
+	# checks to see if 'www.' is in front of domain.
+	# if so removes it from the domain.
+	if domain[:4] == "www.":
+		domain = domain[4:]
+
 	domain = "/" + domain
 	return domain
 
