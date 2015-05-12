6ab32cf70eee41a71bd6e7bc63211bee30bab365
Hayley Denbraver <runningwendybird@gmail.com>
2015-05-12 16:18:57 -0700

    Using better tools for parsing. Improved results because now I am not getting links from the header.

--- parse_html.py
+++ parse_html.py
@@ -1,30 +1,37 @@
+from bs4 import BeautifulSoup
 import urllib2
-import re
-import urlparse
+from tld import get_tld
 
 def return_links(my_url):
 	"""Opens url, reads it, finds the links using 
 	regular expressions and returns them. Links are not sorted."""
 	s = urllib2.urlopen(my_url)
 	s = s.read()
-	urls = re.findall(r'href=[\'"]?([^\'" >]+)', s)
+	soup = BeautifulSoup(s)
+	urls = []
+	for link in soup.find_all('a'):
+		urls.append(link.get('href'))
 	return urls
 
 def determine_domain(my_url):
 	"""Returns the domain for a given url."""
-	domain = urlparse.urlparse(my_url)[1]
-	# checks to see if 'www.' is in front of domain.
-	# if so removes it from the domain.
-	if domain[:4] == "www.":
-		domain = domain[4:]
-
+	domain = get_tld(my_url)
 	return domain
 
+
+# tests to run with pytest
+
 def test_links():
-	assert return_links("http://twinoaksgrowers.com") == ["/format/standard.css", "/format/print.css", "/products.htm", "/avail/", "/care.htm", "/news.htm", "/contact.htm"] 
+	assert return_links("http://twinoaksgrowers.com") == ["/products.htm", "/avail/", "/care.htm", 
+	"/news.htm", "/contact.htm"] 
 
 def test_domain():
 	assert determine_domain("http://www.twitter.com") == "twitter.com"
 
 def test_links_more_difficult():
-	assert return_links("http://runfreerunme.herokuapp.com") == ["/static/bootstrap/css/bootstrap.min.css", "http://fonts.googleapis.com/css?family=Quando", "/static/winged.ico", "/", "/about", "/business", "/about", "/business", "http://www.github.com/runningwendybird/runfree", "/demo", "http://www.github.com/runningwendybird", "http://www.twitter.com/runwendybird", "https://www.linkedin.com/in/hayleydenbraverpe", "mailto:hayley.denbraver@gmail.com", "/about", "/new_user", "www.hackbrightacademy.com", "http://www.github.com/runningwendybird", "http://www.twitter.com/runwendybird", "https://www.linkedin.com/in/hayleydenbraverpe", "mailto:hayley.denbraver@gmail.com"]
+	assert return_links("http://runfreerunme.herokuapp.com") == ["/", "/about", "/business", "/about", 
+	"/business", "http://www.github.com/runningwendybird/runfree", "/demo", "http://www.github.com/runningwendybird",
+	"http://www.twitter.com/runwendybird", "https://www.linkedin.com/in/hayleydenbraverpe", 
+	"mailto:hayley.denbraver@gmail.com", "/about", "/new_user", "www.hackbrightacademy.com", 
+	"http://www.github.com/runningwendybird", "http://www.twitter.com/runwendybird", 
+	"https://www.linkedin.com/in/hayleydenbraverpe", "mailto:hayley.denbraver@gmail.com"]
